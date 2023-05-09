b6887af0e507a165be0c86db7f5f67e43dd19e10
Amy Hung <biboamy@Amys-MacBook-Air.local>
2023-05-09 15:04:22 -0700

    fix: style

--- training/data_loader/gtzan_loader.py
+++ training/data_loader/gtzan_loader.py
@@ -7,54 +7,77 @@ import soundfile as sf
 
 
 class GTZAN(data.Dataset):
-	def __init__(self, root, split, input_length=None):
-		split = split.lower()
-		self.mappeing = {'blues': 0, 'classical': 1, 'country': 2, 'disco': 3, 'hiphop': 4, 'jazz': 5, 'metal': 6, 'pop': 7, 'reggae': 8, 'rock': 9}
-		self.files = open(f"{root}/{split}_filtered.txt", 'r').readlines()
-		self.class_num = 10
-		self.split = split
-		self.seg_length = input_length
-		self.root = root
-		
-	def __len__(self):
-		if self.split == 'train':
-			return 10000
-		else:
-			return len(self.files)
+    def __init__(self, root, split, input_length=None):
+        split = split.lower()
+        self.mappeing = {
+            "blues": 0,
+            "classical": 1,
+            "country": 2,
+            "disco": 3,
+            "hiphop": 4,
+            "jazz": 5,
+            "metal": 6,
+            "pop": 7,
+            "reggae": 8,
+            "rock": 9,
+        }
+        self.files = [
+            f
+            for f in open(f"{root}/{split}_filtered.txt", "r").readlines()
+            if "jazz.00054" not in f
+        ]
+        self.class_num = 10
+        self.split = split
+        self.seg_length = input_length
+        self.root = root
 
-	def __getitem__(self, idx):
-		if self.split == 'train':
-			idx = random.randint(0, len(self.files)-1)
-		file = self.files[idx].strip()
-		frame = sf.info(os.path.join(self.root, file)).frames
-		label = np.zeros(self.class_num)
-		label[self.mappeing[file.split('/')[0]]] = 1
-		if self.split == 'train':
-			start = random.randint(0, frame-self.seg_length-16000)
-			
-			end = start + self.seg_length
-			audio, sr = sf.read(os.path.join(self.root, file), start=start, stop=end)
-			audio = audio.astype('float32')
-			
-			return audio, label.astype('float32')
-		else:
-			audio, sr = sf.read(os.path.join(self.root, file))
-			audio = audio.astype('float32')
-			
-			n_chunk = len(audio) // self.seg_length 
-			
-			audio_chunks = np.split(audio[:int(n_chunk*self.seg_length)], n_chunk)
-			audio_chunks.append(audio[-int(self.seg_length):])
-			audio = np.array(audio_chunks)
+    def __len__(self):
+        if self.split == "train":
+            return 10000
+        else:
+            return len(self.files)
 
-			return audio, label.astype('float32')
+    def __getitem__(self, idx):
+        if self.split == "train":
+            idx = random.randint(0, len(self.files) - 1)
+        file = self.files[idx].strip()
+        frame = sf.info(os.path.join(self.root, file)).frames
+        label = np.zeros(self.class_num)
+        label[self.mappeing[file.split("/")[0]]] = 1
+        if self.split == "train":
+            start = random.randint(0, frame - self.seg_length - 16000)
+
+            end = start + self.seg_length
+            audio, sr = sf.read(os.path.join(self.root, file),
+                                start=start, stop=end)
+            audio = audio.astype("float32")
+            return audio, label.astype("float32")
+        else:
+            audio, sr = sf.read(os.path.join(self.root, file))
+            audio = audio.astype("float32")
+
+            n_chunk = len(audio) // self.seg_length
+
+            audio_chunks = np.split(audio[: int(n_chunk * self.seg_length)],
+                                    n_chunk)
+            audio_chunks.append(audio[-int(self.seg_length):])
+            audio = np.array(audio_chunks)
+
+            return audio, label.astype("float32")
 
 
-def get_audio_loader(root, batch_size, split='TRAIN', num_workers=0, input_length=None):
-	data_loader = data.DataLoader(dataset=GTZAN(root, split=split, input_length=input_length),
-								  batch_size=batch_size,
-								  shuffle=True,
-								  drop_last=True,
-								  num_workers=num_workers)
-	return data_loader
-
+def get_audio_loader(
+    root,
+    batch_size,
+    split="TRAIN",
+    num_workers=0,
+    input_length=None
+):
+    data_loader = data.DataLoader(
+        dataset=GTZAN(root, split=split, input_length=input_length),
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=num_workers,
+    )
+    return data_loader
--- training/main.py
+++ training/main.py
@@ -11,8 +11,9 @@ def fix_seed(seed):
     torch.cuda.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
-    torch.backends.cudnn.enabled=False
-    torch.backends.cudnn.deterministic=True
+    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.deterministic = True
+
 
 def main(config):
     # path for models
@@ -22,42 +23,62 @@ def main(config):
     fix_seed(1234)
 
     # import data loader
-    if config.dataset == 'gtzan':
+    if config.dataset == "gtzan":
         from data_loader.gtzan_loader import get_audio_loader
-   
+
     # get data loder
-    train_loader = get_audio_loader(config.data_path,
-                                    config.batch_size,
-                                    split='TRAIN',
-                                    input_length=config.input_length,
-                                    num_workers=config.num_workers)
-    valid_loader = get_audio_loader(config.data_path,
-                                    1,
-                                    split='valid',
-                                    input_length=config.input_length,
-                                    num_workers=config.num_workers)
+    train_loader = get_audio_loader(
+        config.data_path,
+        config.batch_size,
+        split="TRAIN",
+        input_length=config.input_length,
+        num_workers=config.num_workers,
+    )
+    valid_loader = get_audio_loader(
+        config.data_path,
+        1,
+        split="valid",
+        input_length=config.input_length,
+        num_workers=config.num_workers,
+    )
     solver = Solver(train_loader, valid_loader, config)
     solver.train()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
-    parser.add_argument('--num_workers', type=int, default=6)
-    parser.add_argument('--dataset', type=str, default='gtzan', choices=['gtzan'])
-    parser.add_argument('--model_type', type=str, default='ast',
-                        choices=['CNN235.5k', 'speechatt', 'ast'])
-    parser.add_argument('--n_epochs', type=int, default=200)
-    parser.add_argument('--batch_size', type=int, default=16)
-    parser.add_argument('--lr', type=float, default=1e-4)
-    parser.add_argument('--use_tensorboard', type=int, default=1)
-    parser.add_argument('--model_save_path', type=str, default='../models')
-    parser.add_argument('--data_path', type=str, default='../data')
-    parser.add_argument('--log_step', type=int, default=20)
-    parser.add_argument('--input_length', type=int, default=160000)
-    parser.add_argument('--map_num', type=int, default=5)
-    parser.add_argument('--pad_num', type=int, default=500)
-    parser.add_argument('--reprog_front', type=str, default=['None'], choices=['None', 'uni_noise', 'condi', 'skip'])
+    parser.add_argument("--num_workers", type=int, default=6)
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="gtzan",
+        choices=["gtzan"]
+    )
+    parser.add_argument(
+        "--model_type",
+        type=str,
+        default="ast",
+        choices=["CNN235.5k", "speechatt", "ast"],
+    )
+    parser.add_argument("--n_epochs", type=int, default=200)
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--use_tensorboard", type=int, default=1)
+    parser.add_argument("--model_save_path", type=str, default="../models")
+    parser.add_argument("--data_path", type=str, default="../data")
+    parser.add_argument("--log_step", type=int, default=20)
+    parser.add_argument("--input_length", type=int, default=160000)
+    parser.add_argument("--map_num", type=int, default=5)
+    parser.add_argument("--pad_num", type=int, default=500)
+    parser.add_argument(
+        "--reprog_front",
+        type=str,
+        default=["None"],
+        choices=["None", "uni_noise", "condi", "skip"],
+    )
 
     config = parser.parse_args()
 
