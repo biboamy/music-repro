d99081aef8169f9d77370a38d4e99607c36142d5
Amy Hung <biboamy@Amys-MacBook-Air.local>
2023-05-09 15:04:22 -0700

    fix: style

--- training/modules.py
+++ training/modules.py
@@ -1,11 +1,7 @@
 import numpy as np
 import torch
-import torch.nn.functional as F
 import torch.nn as nn
 import torchaudio
-import sys
-from torch.autograd import Variable
-import math
 import librosa
 
 
@@ -13,19 +9,23 @@ import librosa
 def hz_to_midi(hz):
     return 12 * (torch.log2(hz) - np.log2(440.0)) + 69
 
+
 def midi_to_hz(midi):
-    return 440.0 * (2.0 ** ((midi - 69.0)/12.0))
+    return 440.0 * (2.0 ** ((midi - 69.0) / 12.0))
+
 
 def note_to_midi(note):
     return librosa.core.note_to_midi(note)
 
+
 def hz_to_note(hz):
     return librosa.core.hz_to_note(hz)
+
 
 def initialize_filterbank(sample_rate, n_harmonic, semitone_scale):
     # MIDI
     # lowest note
-    low_midi = note_to_midi('C1')
+    low_midi = note_to_midi("C1")
 
     # highest note
     high_note = hz_to_note(sample_rate / (2 * n_harmonic))
@@ -39,24 +39,26 @@ def initialize_filterbank(sample_rate, n_harmonic, semitone_scale):
     # stack harmonics
     harmonic_hz = []
     for i in range(n_harmonic):
-        harmonic_hz = np.concatenate((harmonic_hz, hz * (i+1)))
+        harmonic_hz = np.concatenate((harmonic_hz, hz * (i + 1)))
 
     return harmonic_hz, level
 
 
 class HarmonicSTFT(nn.Module):
-    def __init__(self,
-                 sample_rate=16000,
-                 n_fft=1024,
-                 win_length=None,
-                 hop_length=None,
-                 pad=0,
-                 power=1,
-                 normalized=False,
-                 n_harmonic=6,
-                 semitone_scale=2,
-                 bw_Q=1.0,
-                 learn_bw=None):
+    def __init__(
+        self,
+        sample_rate=16000,
+        n_fft=1024,
+        win_length=None,
+        hop_length=None,
+        pad=0,
+        power=1,
+        normalized=False,
+        n_harmonic=6,
+        semitone_scale=2,
+        bw_Q=1.0,
+        learn_bw=None,
+    ):
         super(HarmonicSTFT, self).__init__()
 
         # Parameters
@@ -66,33 +68,43 @@ class HarmonicSTFT(nn.Module):
         self.bw_beta = 24.7
 
         # Spectrogram
-        self.spec = torchaudio.transforms.Spectrogram(n_fft=n_fft, win_length=win_length,
-                                                      hop_length=None, pad=0,
-                                                      window_fn=torch.hann_window,
-                                                      power=power, normalized=normalized, wkwargs=None)
+        self.spec = torchaudio.transforms.Spectrogram(
+            n_fft=n_fft,
+            win_length=win_length,
+            hop_length=None,
+            pad=0,
+            window_fn=torch.hann_window,
+            power=power,
+            normalized=normalized,
+            wkwargs=None,
+        )
         self.amplitude_to_db = torchaudio.transforms.AmplitudeToDB()
 
         # Initialize the filterbank. Equally spaced in MIDI scale.
-        harmonic_hz, self.level = initialize_filterbank(sample_rate, n_harmonic, semitone_scale)
+        harmonic_hz, self.level = initialize_filterbank(
+            sample_rate, n_harmonic, semitone_scale
+        )
 
         # Center frequncies to tensor
-        self.f0 = torch.tensor(harmonic_hz.astype('float32'))
+        self.f0 = torch.tensor(harmonic_hz.astype("float32"))
 
         # Bandwidth parameters
-        if learn_bw == 'only_Q':
-            self.bw_Q = nn.Parameter(torch.tensor(np.array([bw_Q]).astype('float32')))
-        elif learn_bw == 'fix':
-            self.bw_Q = torch.tensor(np.array([bw_Q]).astype('float32'))
+        if learn_bw == "only_Q":
+            self.bw_Q = nn.Parameter(
+                torch.tensor(np.array([bw_Q]).astype("float32"))
+            )
+        elif learn_bw == "fix":
+            self.bw_Q = torch.tensor(np.array([bw_Q]).astype("float32"))
 
     def get_harmonic_fb(self):
         # bandwidth
         bw = (self.bw_alpha * self.f0 + self.bw_beta) / self.bw_Q
-        bw = bw.unsqueeze(0) # (1, n_band)
-        f0 = self.f0.unsqueeze(0) # (1, n_band)
-        fft_bins = self.fft_bins.unsqueeze(1) # (n_bins, 1)
+        bw = bw.unsqueeze(0)  # (1, n_band)
+        f0 = self.f0.unsqueeze(0)  # (1, n_band)
+        fft_bins = self.fft_bins.unsqueeze(1)  # (n_bins, 1)
 
-        up_slope = torch.matmul(fft_bins, (2/bw)) + 1 - (2 * f0 / bw)
-        down_slope = torch.matmul(fft_bins, (-2/bw)) + 1 + (2 * f0 / bw)
+        up_slope = torch.matmul(fft_bins, (2 / bw)) + 1 - (2 * f0 / bw)
+        down_slope = torch.matmul(fft_bins, (-2 / bw)) + 1 + (2 * f0 / bw)
         fb = torch.max(self.zero, torch.min(down_slope, up_slope))
         return fb
 
@@ -100,7 +112,7 @@ class HarmonicSTFT(nn.Module):
         self.f0 = self.f0.to(device)
         self.bw_Q = self.bw_Q.to(device)
         # fft bins
-        self.fft_bins = torch.linspace(0, self.sample_rate//2, n_bins)
+        self.fft_bins = torch.linspace(0, self.sample_rate // 2, n_bins)
         self.fft_bins = self.fft_bins.to(device)
         self.zero = torch.zeros(1)
         self.zero = self.zero.to(device)
@@ -114,11 +126,18 @@ class HarmonicSTFT(nn.Module):
 
         # triangle filter
         harmonic_fb = self.get_harmonic_fb()
-        harmonic_spec = torch.matmul(spectrogram.transpose(1, 2), harmonic_fb).transpose(1, 2)
+        harmonic_spec = torch.matmul(
+            spectrogram.transpose(1, 2), harmonic_fb
+        ).transpose(1, 2)
 
         # (batch, channel, length) -> (batch, harmonic, f0, length)
-        b, c, l = harmonic_spec.size()
-        harmonic_spec = harmonic_spec.view(b, self.n_harmonic, self.level, l)
+        b, c, time = harmonic_spec.size()
+        harmonic_spec = harmonic_spec.view(
+            b,
+            self.n_harmonic,
+            self.level,
+            time
+        )
 
         # amplitude to db
         harmonic_spec = self.amplitude_to_db(harmonic_spec)
--- training/solver.py
+++ training/solver.py
@@ -8,10 +8,8 @@ import sys
 import tqdm
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 from torch.autograd import Variable
-import soundfile as sf
 
 
 class Solver(object):
@@ -30,7 +28,7 @@ class Solver(object):
         self.map_num = config.map_num
         self.pad_num = config.pad_num
         self.reprog_front = config.reprog_front
-        if self.dataset == 'gtzan':
+        if self.dataset == "gtzan":
             self.n_class = 10
 
         # model path and step size
@@ -45,25 +43,36 @@ class Solver(object):
         # Build model
         self.build_model()
 
-        model_parameters = filter(lambda p: p.requires_grad, self.model.parameters())
+        model_parameters = filter(lambda p: p.requires_grad,
+                                  self.model.parameters())
         params = sum([np.prod(p.size()) for p in model_parameters])
-        print(f'Trainable parameters: {str(params)}')
+        print(f"Trainable parameters: {str(params)}")
 
         # Tensorboard
         self.writer = SummaryWriter()
 
-
     def get_model(self):
-        if self.model_type in ['CNN235.5k']:
+        if self.model_type in ["CNN235.5k"]:
             from models.CNNModel import CNNModel
-            return CNNModel(model_type=self.model_type, n_class=self.n_class)
-        elif self.model_type == 'speechatt':
-            from models.SpeechModel import V2SReprogModel
-            return V2SReprogModel(map_num=self.map_num, n_class=self.n_class, reprog_front=self.reprog_front, is_cuda=self.is_cuda)
-        elif self.model_type == 'ast':
-            from models.ASTModel import AST
-            return AST(n_class=self.n_class, reprog_front=self.reprog_front, map_num=self.map_num)
 
+            return CNNModel(model_type=self.model_type, n_class=self.n_class)
+        elif self.model_type == "speechatt":
+            from models.SpeechModel import V2SReprogModel
+
+            return V2SReprogModel(
+                map_num=self.map_num,
+                n_class=self.n_class,
+                reprog_front=self.reprog_front,
+                is_cuda=self.is_cuda,
+            )
+        elif self.model_type == "ast":
+            from models.ASTModel import AST
+
+            return AST(
+                n_class=self.n_class,
+                reprog_front=self.reprog_front,
+                map_num=self.map_num,
+            )
 
     def build_model(self):
         # model
@@ -73,7 +82,9 @@ class Solver(object):
         if self.is_cuda:
             self.model.cuda()
 
-        self.optimizer = torch.optim.Adam(self.model.parameters(), self.lr, weight_decay=1e-4)
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(), self.lr, weight_decay=1e-4
+        )
 
     def to_var(self, x):
         if isinstance(x, dict):
@@ -92,7 +103,7 @@ class Solver(object):
     def train(self):
         # Start training
         start_t = time.time()
-        current_optimizer = 'adam'
+        current_optimizer = "adam"
         reconst_loss = self.get_loss_function()
         best_metric = 0
         drop_counter = 0
@@ -102,11 +113,10 @@ class Solver(object):
             ctr = 0
             drop_counter += 1
             self.model = self.model.train()
-           
+
             for x, y in self.data_loader:
                 ctr += 1
                 # Forward
-                b_size = len(x)
                 x = self.to_var(x)
                 y = self.to_var(y)
                 out = self.model(x)
@@ -118,93 +128,113 @@ class Solver(object):
 
                 # Log
                 self.print_log(epoch, ctr, loss, start_t)
-            self.writer.add_scalar('Loss/train', loss.item(), epoch)
+            self.writer.add_scalar("Loss/train", loss.item(), epoch)
 
             # validation
             best_metric = self.validation(best_metric, epoch)
 
             # schedule optimizer
-            current_optimizer, drop_counter = self.opt_schedule(current_optimizer, drop_counter)
+            current_optimizer, drop_counter = self.opt_schedule(
+                current_optimizer, drop_counter
+            )
 
-        print("[%s] Train finished. Elapsed: %s"
-                % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                    datetime.timedelta(seconds=time.time() - start_t)))
+        print(
+            "[%s] Train finished. Elapsed: %s"
+            % (
+                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                datetime.timedelta(seconds=time.time() - start_t),
+            )
+        )
 
     def load(self, filename):
         S = torch.load(filename)
         model_dict = self.model.state_dict()
-        pretrained_dict = {k: v for k, v in S.items() if k in model_dict and 'delta' not in k}
-        model_dict.update(pretrained_dict) 
+        pretrained_dict = {
+            k: v for k, v in S.items() if k in model_dict and "delta" not in k
+        }
+        model_dict.update(pretrained_dict)
         self.model.load_state_dict(model_dict)
-
 
     def opt_schedule(self, current_optimizer, drop_counter):
         # adam to sgd
-        if current_optimizer == 'adam' and drop_counter == 80:
-            self.load(os.path.join(self.model_save_path, 'best_model.pth'))
-            self.optimizer = torch.optim.SGD(self.model.parameters(), 0.001,
-                                            momentum=0.9, weight_decay=0.0001,
-                                            nesterov=True)
-            current_optimizer = 'sgd_1'
+        if current_optimizer == "adam" and drop_counter == 80:
+            self.load(os.path.join(self.model_save_path, "best_model.pth"))
+            self.optimizer = torch.optim.SGD(
+                self.model.parameters(),
+                0.001,
+                momentum=0.9,
+                weight_decay=0.0001,
+                nesterov=True,
+            )
+            current_optimizer = "sgd_1"
             drop_counter = 0
-            print('sgd 1e-3')
+            print("sgd 1e-3")
         # first drop
-        if current_optimizer == 'sgd_1' and drop_counter == 50:
-            self.load(os.path.join(self.model_save_path, 'best_model.pth'))
+        if current_optimizer == "sgd_1" and drop_counter == 50:
+            self.load(os.path.join(self.model_save_path, "best_model.pth"))
             for pg in self.optimizer.param_groups:
-                pg['lr'] = 0.0005
-            current_optimizer = 'sgd_2'
+                pg["lr"] = 0.0005
+            current_optimizer = "sgd_2"
             drop_counter = 0
-            print('sgd 5e-4')
-        
+            print("sgd 5e-4")
+
         # second drop
-        if current_optimizer == 'sgd_2' and drop_counter == 20:
-            self.load(os.path.join(self.model_save_path, 'best_model.pth'))
+        if current_optimizer == "sgd_2" and drop_counter == 20:
+            self.load(os.path.join(self.model_save_path, "best_model.pth"))
             for pg in self.optimizer.param_groups:
-                pg['lr'] = 0.00001
-            current_optimizer = 'sgd_3'
-            print('sgd 1e-5')
-        
+                pg["lr"] = 0.00001
+            current_optimizer = "sgd_3"
+            print("sgd 1e-5")
+
         return current_optimizer, drop_counter
 
     def save(self, filename):
         model = self.model.state_dict()
-        torch.save({'model': model}, filename)
-
+        torch.save({"model": model}, filename)
 
     def get_auc(self, est_array, gt_array):
-        roc_aucs  = metrics.roc_auc_score(gt_array, est_array, average='macro')
-        pr_aucs = metrics.average_precision_score(gt_array, est_array, average='macro')
-        print('roc_auc: %.4f pr_auc: %.4f' % (roc_aucs, pr_aucs))
+        roc_aucs = metrics.roc_auc_score(gt_array, est_array, average="macro")
+        pr_aucs = metrics.average_precision_score(gt_array,
+                                                  est_array,
+                                                  average="macro")
+        print("roc_auc: %.4f pr_auc: %.4f" % (roc_aucs, pr_aucs))
         return roc_aucs, pr_aucs
 
     def get_acc(self, est_array, gt_array):
-        #est_array[est_array>0.5] = 1
-        #est_array[est_array<=0.5] = 0
+        # est_array[est_array>0.5] = 1
+        # est_array[est_array<=0.5] = 0
         acc = metrics.accuracy_score(gt_array, est_array)
-        print('Acc: %.4f' % acc)
+        print("Acc: %.4f" % acc)
         return acc
 
     def print_log(self, epoch, ctr, loss_bce, start_t):
         if (ctr) % self.log_step == 0:
-            sys.stdout.write('\r')
-            sys.stdout.write("[%s] Epoch [%d/%d] Iter [%d/%d] train bce loss: %.4f Elapsed: %s" %
-                    (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                        epoch+1, self.n_epochs, ctr, len(self.data_loader), loss_bce.item(),
-                        datetime.timedelta(seconds=time.time()-start_t)))
+            sys.stdout.write("\r")
+            sys.stdout.write(
+                "[%s] Epoch [%d/%d] Iter [%d/%d] train bce loss: %.4f Time: %s"
+                % (
+                    datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    epoch + 1,
+                    self.n_epochs,
+                    ctr,
+                    len(self.data_loader),
+                    loss_bce.item(),
+                    datetime.timedelta(seconds=time.time() - start_t),
+                )
+            )
             sys.stdout.flush()
-
 
     def validation(self, best_metric, epoch):
         acc, loss = self.get_validation_score(epoch)
         score = acc
         if score > best_metric:
-            print('best model!')
+            print("best model!")
             best_metric = score
-            torch.save(self.model.state_dict(),
-                       os.path.join(self.model_save_path, 'best_model.pth'))
+            torch.save(
+                self.model.state_dict(),
+                os.path.join(self.model_save_path, "best_model.pth"),
+            )
         return best_metric
-
 
     def get_validation_score(self, epoch):
         self.model = self.model.eval()
@@ -220,7 +250,7 @@ class Solver(object):
 
                 y = self.to_var(y).repeat(len(x), 1)
                 out = self.model(x)
-               
+
                 loss = reconst_loss(out, y)
                 losses.append(float(loss.data))
                 out = out.detach().cpu()
@@ -234,13 +264,13 @@ class Solver(object):
 
         est_array, gt_array = np.array(est_array), np.array(gt_array)
         loss = np.mean(losses)
-        print('loss: %.4f' % loss)
+        print("loss: %.4f" % loss)
 
-        acc = self.get_acc(np.argmax(est_array, axis=1), np.argmax(gt_array, axis=1))
+        acc = self.get_acc(np.argmax(est_array, axis=1),
+                           np.argmax(gt_array, axis=1))
         roc_auc, pr_auc = self.get_auc(est_array, gt_array)
-        self.writer.add_scalar('Loss/valid', loss, epoch)
-        self.writer.add_scalar('AUC/ROC', roc_auc, epoch)
-        self.writer.add_scalar('AUC/PR', pr_auc, epoch)
-        self.writer.add_scalar('ACC', acc, epoch)
+        self.writer.add_scalar("Loss/valid", loss, epoch)
+        self.writer.add_scalar("AUC/ROC", roc_auc, epoch)
+        self.writer.add_scalar("AUC/PR", pr_auc, epoch)
+        self.writer.add_scalar("ACC", acc, epoch)
         return acc, loss
-
